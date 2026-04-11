6ae2c542e0216a557bf47942562f58a99441c49d
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-04-11 11:22:48 +0530

    Create Q04.cpp

---
+++ Lab_Manual_Solutions/Q04.cpp
@@ -0,0 +1,8 @@
+/*4. A shopkeeper wants to calculate the total bill amount after applying a 20% discount on the purchase. 
+Implement a solution to accept item no., quantity, and unit price. Compute the amount and apply 20% discount.*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+}
