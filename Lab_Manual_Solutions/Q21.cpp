fe5d70cea4e1eb9b90f56c03f98afc15e6392c14
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-04-19 22:48:46 +0530

    Add solution for Q21: Implement rectangular hollow frame display in CAD tool

---
+++ Lab_Manual_Solutions/Q21.cpp
@@ -0,0 +1,28 @@
+/* Q21. A CAD tool generates rectangular hollow frames. Implement a solution to display:
+********
+*      *
+*      *
+*      *
+*      *
+*      *
+*      *
+******** */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 8, cols = 8;
+
+    for(int i = 1; i <= rows; i++) {
+        for(int j = 1; j <= cols; j++) {
+            if(i == 1 || i == rows || j == 1 || j == cols)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
