ee1cafc5af813363653d5dc3f2625c3c1e8f4334
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-04-19 22:41:03 +0530

    Refactor Q15.cpp: Clean up formatting and improve code readability for maximum sales figure calculation

--- Lab_Manual_Solutions/Q15.cpp
+++ Lab_Manual_Solutions/Q15.cpp
@@ -1,13 +1,23 @@
-/*
-15. A data analytics tool finds the maximum sales figure from multiple entries. 
-Implement a solution to accept ‘n’ numbers and display the largest. 
-*/
+/* Q15. A data analytics tool finds the maximum sales figure from multiple entries. Implement a solution to accept ‘n’ numbers and display the largest. */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+int main() {
+    int n;
+    cin >> n;
+
+    int x, maxVal;
+    cin >> x;
+    maxVal = x;
+
+    for(int i = 2; i <= n; i++) {
+        cin >> x;
+        if(x > maxVal)
+            maxVal = x;
+    }
+
+    cout << maxVal;
 
     return 0;
 }
