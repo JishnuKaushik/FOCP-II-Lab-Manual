d906213c3e7c2c71f427384028b23e34cf21a464
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-04-19 22:14:48 +0530

    SOL FOR QUESTION 04

--- Lab_Manual_Solutions/Q04.cpp
+++ Lab_Manual_Solutions/Q04.cpp
@@ -1,8 +1,20 @@
-/*4. A shopkeeper wants to calculate the total bill amount after applying a 20% discount on the purchase. 
-Implement a solution to accept item no., quantity, and unit price. Compute the amount and apply 20% discount.*/
+/* Q4. A shopkeeper wants to calculate the total bill amount after applying a 20% discount on the purchase. Implement a solution to accept item no., quantity, and unit price. Compute the amount and apply 20% discount. */
 
 #include <iostream>
 using namespace std;
-int main(){
-    
+
+int main() {
+int itemNo;
+float quantity, unitPrice, amount, discount, finalAmount;
+
+cin >> itemNo >> quantity >> unitPrice;
+
+amount = quantity * unitPrice;
+discount = amount * 0.20;
+finalAmount = amount - discount;
+
+cout << finalAmount;
+
+return 0;
+
 }
